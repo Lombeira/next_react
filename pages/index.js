5c6d48d94a4c17686b9176b02e3cfd17e9eacae0
Lombeira <glomenha@gmail.com>
2021-08-25 23:34:57 -0300

    impl: axios pokemon and pagination component

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,53 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
+import Pagination from './Pagination';
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-      <Contador />
-      <div>teste</div>
-    </div>
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    'https://pokeapi.co/api/v2/pokemon'
   );
-}
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
-function Contador() {
-  const [contador, setContador] = useState(1);
-  function adcionarContador() {
-    setContador(contador + 1);
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl);
   }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return 'Loading...';
+
   return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adcionarContador}>Adcionar</button>
-    </div>
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
   );
 }
 
-export default Home;
+export default App;
